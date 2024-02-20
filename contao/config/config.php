50b9ee2ba66a5af39184230b7194e51587af8a77
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2024-02-20 18:12:39 +0100

    state of work

--- contao/config/config.php
+++ contao/config/config.php
@@ -10,6 +10,11 @@
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
 
+use HeimrichHannot\Blocks\ContentElement\ContentBlock;
+use HeimrichHannot\Blocks\Controller\Hooks;
+use HeimrichHannot\Blocks\EventListener\InsertTagsListener;
+use HeimrichHannot\Blocks\Model\BlockModel;
+use HeimrichHannot\Blocks\Model\BlockModuleModel;
 use HeimrichHannot\Blocks\Module\BlockModule;
 
 /**
@@ -22,8 +27,8 @@ $GLOBALS['BE_MOD']['design']['themes']['tables'][] = 'tl_content';
 /**
  * Models
  */
-$GLOBALS['TL_MODELS']['tl_block']        = 'HeimrichHannot\Blocks\Model\BlockModel';
-$GLOBALS['TL_MODELS']['tl_block_module'] = 'HeimrichHannot\Blocks\Model\BlockModuleModel';
+$GLOBALS['TL_MODELS']['tl_block']        = BlockModel::class;
+$GLOBALS['TL_MODELS']['tl_block_module'] = BlockModuleModel::class;
 
 /**
  * Front end modules
@@ -33,25 +38,25 @@ array_splice($GLOBALS['FE_MOD']['miscellaneous'], 0, 0, [BlockModule::TYPE => Bl
 /**
  * Content elements
  */
-$GLOBALS['TL_CTE']['includes']['block'] = '\HeimrichHannot\Blocks\ContentElement\ContentBlock';
+$GLOBALS['TL_CTE']['includes']['block'] = ContentBlock::class;
 
-/**
- * Easy Themes Support
- */
-
-$GLOBALS['TL_EASY_THEMES_MODULES'] = array_merge
-(
-    [
-        'blocks' => [
-            'href_fragment' => 'table=tl_block',
-            'icon'          => 'system/modules/blocks/assets/icon.png',
-        ],
-    ],
-    isset($GLOBALS['TL_EASY_THEMES_MODULES']) && is_array($GLOBALS['TL_EASY_THEMES_MODULES']) ? $GLOBALS['TL_EASY_THEMES_MODULES'] : []
-);
+// /**
+//  * Easy Themes Support
+//  */
+//
+// $GLOBALS['TL_EASY_THEMES_MODULES'] = array_merge
+// (
+//     [
+//         'blocks' => [
+//             'href_fragment' => 'table=tl_block',
+//             'icon'          => 'system/modules/blocks/assets/icon.png',
+//         ],
+//     ],
+//     isset($GLOBALS['TL_EASY_THEMES_MODULES']) && is_array($GLOBALS['TL_EASY_THEMES_MODULES']) ? $GLOBALS['TL_EASY_THEMES_MODULES'] : []
+// );
 
 /**
  * Hooks
  */
-$GLOBALS['TL_HOOKS']['generateBreadcrumb']['huh.blocks'] = ['HeimrichHannot\Blocks\Controller\Hooks', 'generateBreadcrumbHook'];
-$GLOBALS['TL_HOOKS']['replaceInsertTags']['huh.blocks'] = ['HeimrichHannot\Blocks\EventListener\InsertTagsListener', 'onReplaceInsertTags'];
+$GLOBALS['TL_HOOKS']['generateBreadcrumb']['huh.blocks'] = [Hooks::class, 'generateBreadcrumbHook'];
+$GLOBALS['TL_HOOKS']['replaceInsertTags']['huh.blocks'] = [InsertTagsListener::class, 'onReplaceInsertTags'];
